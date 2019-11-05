9cac868e58629f30f1a78e0be8c5c5775c0789fc
bradnielsen2981 <38126504+bradnielsen2981@users.noreply.github.com>
2019-11-05 14:51:16 +1000

    creating a generic ajax script

--- static/script.js
+++ static/script.js
@@ -1,7 +1,9 @@
+//need to import jquery scripts
+
+//simple ajax get request
 function ajaxRequest(url) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
-
     xhr.onload = function() {
         if (xhr.status === 200) {
             console.log('Response Text: ' + xhr.responseText);
@@ -11,4 +13,20 @@ function ajaxRequest(url) {
         }
     };
     xhr.send();
+}
+
+//url, method="GET"or"POST", parameterobject=javascript object, crossdomain=true or false, 
+function JQUERYajaxRequest(urlstring, methodstring, parametersobject, crossdomainbool, responsehandler)
+{
+    $.ajax({
+	    type: method,
+        crossDomain: crossdomainbool, //THIS IS REQUIRED IF COMMUNICATING TO NON LOCAL SERVER
+	    url: url,
+	    data: parametersobject, //{ var1:1,var2:"hello"  }
+        dataType : "json",  //calls json.parse to convert results (JSON) to JavascriptObject
+        success: responsehandler(results),
+        error: function(error) {
+           	console.log(error);
+	    }
+    });
 }
